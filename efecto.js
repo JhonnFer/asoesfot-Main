bb4c4c17b9135c1332159b2c6987f99f205346f4
JhonnFer <jhoncasq23@gmail.com>
2024-12-17 06:44:46 -0500

    menu hamburguesa

--- efecto.js
+++ efecto.js
@@ -14,6 +14,4 @@ const typewriter = (text, elementId, speed) => {
     type();
 };
 
-// Texto que deseas mostrar
-const message = "Bienvenidos a ASOESFOT.";
-typewriter(message, "typed", 100); // Llama a la función
+
